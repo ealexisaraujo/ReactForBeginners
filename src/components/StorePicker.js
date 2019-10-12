73ad9d159c35599d935024a248d1684ba426e685
Alexis Araujo <ala1289@gmail.com>
2019-10-11 23:06:37 -0500

    Events, Refs and this Binding

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { getFunName } from '../helpers';
 
-const StorePicker = () => (
-  <form className='store-selector'>
-    <h2>Please Enter a Store</h2>
-    <input
-      type='text'
-      required
-      placeholder='Store Name'
-      defaultValue={getFunName()}
-    />
-    <button type='submit'>Visit store ⟶</button>
-  </form>
-);
+const StorePicker = () => {
+  const [form, setValues] = useState({
+    email: ''
+  });
+
+  const handleInput = event => {
+    setValues({
+      ...form,
+      [event.target.name]: event.target.value
+    });
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log(form);
+  };
+  return (
+    <form className='store-selector' onSubmit={handleSubmit}>
+      <h2>Please Enter a Store</h2>
+      <input
+        type='text'
+        required
+        placeholder='Store Name'
+        defaultValue={getFunName()}
+        onChange={handleInput}
+      />
+      <button type='submit'>Visit store ⟶</button>
+    </form>
+  );
+};
 
 export default StorePicker;
