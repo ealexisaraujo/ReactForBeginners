41bbc98ccfee5943838d533f0f966edfd6f9c685
Alexis Araujo <ala1289@gmail.com>
2019-10-11 22:34:04 -0500

    Helpers and utility functions

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 const StorePicker = () => (
   <form className='store-selector'>
     <h2>Please Enter a Store</h2>
-    <input type='text' required placeholder='Store Name' />
+    <input
+      type='text'
+      required
+      placeholder='Store Name'
+      defaultValue={getFunName()}
+    />
     <button type='submit'>Visit store ⟶</button>
   </form>
 );
